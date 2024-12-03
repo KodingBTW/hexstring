04c8b9f3ac18170e5c5aaef15e3a99199436213d
Rodrigo Muñoz <83840135+KodingBTW@users.noreply.github.com>
2024-12-03 17:08:36 -0300

    Update

--- README.txt
+++ README.txt
@@ -1,10 +1,11 @@
 ------------------------------------------------------
 
-Nombre:   		HexString
-Autor:     		koda
-Latest Version:  	1.0
+Nombre:			HexString
+Autor:			koda
+Latest Version:  	1.1.0
 URL:			https://traduccioneskoda.blogspot.com/
-Last Update:    23/11/2024
+				https://github.com/KodingBTW/hexstring
+Last Update:	03/12/2024
 
 ------------------------------------------------------
 1 - WHAT'S THIS?
@@ -20,8 +21,55 @@ focus on translating.
 ------------------------------------------------------
 2 - WHAT'S NEW
 ------------------------------------------------------
+V1.1.0
 
-V1.0
+Bugs Fixed:
+- If you split the text when editing, it was 
+misinterpreted by the encoder as a new pointer. 
+(Thanks to Wave).
+
+- The text block counter function is now smarter. 
+If two pointers point to the same text it will be 
+counted only once.
+
+New features:
+- Added text comments, use ";" at begining of a new 
+line. (it still can be used at character)
+
+- line with @ or | will be ignore too
+
+- Now more exceptions are handled, and an error 
+text will be displayed giving more information.
+
+- Characters not found in the .tbl file will now be 
+printed in the following ~hex~ format. (The "~" symbol
+is reserved, and will be ignored if used in the 
+tbl).
+
+-In the same way when encoding, if ~hex~ is found
+it will be encoded with its corresponding hex form. 
+If any character in the text is not assigned to the 
+dictionary, it will be copied into its ASCCI format.
+
+- Added support for pointers of other formats and 
+lengths.
+	2bytes little endian
+	2bytes big endian
+	2bytes splitted (lsb-msb)
+	3bytes (gba format)
+	4bytes (mega drive - big endian)
+
+- Now when decoding the text, a comment will be 
+automatically created that contains: the address of 
+the line, a copy of the text, character length.
+
+- Added previous pointer copy functionality, just 
+deletes the line and adds the "&" character to the 
+start of the line, then add his line breaker. The
+pointer will be the same as the previous one, very 
+useful if several pointers point to the same line.
+
+V1.0.0
 - Decoding ROM Data.
 - Encoding binary files.
 - Automatic updates pointers table (Only 2 bytes).
@@ -38,6 +86,14 @@ To decode:
 Use the Windows console and type -d, then fill in the 
 arguments:
 
+- pointersFormat:
+
+	-2b   --2bytes "little endian"
+	-2bb  --2bytes "big endian"
+	-2bs  --2bytes "splitted lsb-msb"
+	-3b   --3bytes "(bank/2bytespointer) gba format"
+	-4b   --4bytes "Mega drive games little endian"
+
 - romFile: ROM file name.
 
 - pointersStartAddress: Address where the pointer table 
@@ -45,9 +101,16 @@ begins.
 
 - pointerTableSize: Size of the pointers, in hexadecimal.
 
-- headerSize: The size of the header (Formula: take the 
-first pointer in the table, reverse it, and subtract 
-the address where the text begins) in hexadecimal.
+- headerSize: The size of the header 
+
+Formula: Text Address for the pointer - pointer inverted
+(little endian case)
+
+Example: for Goof Troop (U).snes
+
+The pointer format are 2bytes little endian, so the
+pointer is "81 E8", the text address is "0x05E881", so
+if you use the formula: 5E881 - E881 = 0x50000
 
 - lineBreaker: Code used in the ROM to break the text 
 strings, e.g., 0x00 or 0xFF. (If there is more than 
@@ -67,6 +130,14 @@ To encode:
 Use the Windows console and type -e, then fill in the 
 arguments:
 
+- pointersFormat:
+
+	-2b   --2bytes "little endian"
+	-2bb  --2bytes "big endian"
+	-2bs  --2bytes "splitted lsb-msb"
+	-3b   --3bytes "(bank/2bytespointer) gba format"
+	-4b   --4bytes "Mega drive games little endian"
+
 - textFile: File with the text.
 
 - textStartAddress: Address where the text block begins.
@@ -76,9 +147,7 @@ arguments:
 - pointersStartAddress: Address where the pointer table 
 begins.
 
-- headerSize: The size of the header (Formula: take the
-first pointer in the table, reverse it, and subtract the 
-address where the text begins) in hexadecimal.
+- headerSize: The same decoder formula.
 
 - romFile: ROM file name.
 
@@ -93,20 +162,20 @@ creating a .bat file for quicker operation.
 -Before you begin, it's recommended to make a copy of the
  ROM and back up any progress you have.
  
--You can dump the text and then reinsert it. Use a 
-hexadecimal comparator to check for discrepancies.
+-Before starting to translate, once the text has been 
+extracted, reinsert it again. This will help you know 
+if you are giving the right arguments. You can also use 
+a hexagecimal comparator and compare it with a made copy.
 
--If you notice that the text size exceeds the available 
-space in the text block when reinserting, you can search 
-for empty space in the ROM or simply increase the ROM size.
-
+- If when inserting the text you enter it is greater 
+than the block. You have three options, reduce the text, 
+find an empty space, or expand the rom.
 
 ------------------------------------------------------
 4 - TO DO:
 ------------------------------------------------------
 
-- I assume at some point I’ll add support for 3-byte 
- pointers.
+- Possibly add new algorithms for new pointer formats.
 
 - A graphical interface—unlikely, as it requires a 
 lot of work, which I don't have time for right now.
--- main.py
+++ main.py
@@ -1,87 +1,176 @@
 ## HexString
 ## Source code by koda
-## release 23/11/2024 --version 1.0
+## release 03/12/2024 --version 1.1
 
 import sys
 import os
 import decoder as de
 import encoder as en
 
-def main():
-    
-    if len(sys.argv) not in [8, 9]:
-        sys.stdout.write("Usage: -d <romFile> <PointersStartAddress> <PointerTableSize> <HeaderSize> <LineBreaker> <outFile> [tblFile]\n")
-        sys.stdout.write("       -e <TextFile> <TextStartAddress> <TextSize> <PointersStartAddress> <HeaderSize> <romFile> [tblFile]\n")
-        sys.stdout.write("       -h show help.\n")
-        sys.stdout.write("       -v show version.\n")
-        sys.exit(1)
+def showHelp():
+    sys.stdout.write("Usage: -d <pointersFormat> <romFile> <PointersStartAddress> <PointerTableSize> <HeaderSize> <LineBreaker> <outFile> <tblFile>\n")
+    sys.stdout.write("       -e <pointersFormat> <TextFile> <TextStartAddress> <TextSize> <PointersStartAddress> <HeaderSize> <romFile> <tblFile>\n")
+    sys.stdout.write("       -h show help.\n")
+    sys.stdout.write("       -v show version.\n")
 
+def main():
+    if len(sys.argv) == 1 or len(sys.argv) == 9:
+        showHelp()
+        sys.exit(1)
+    # Decoding arguments
     if sys.argv[1] == '-d':
-        
-        # Decoding arg.
-        romFile = sys.argv[2]                                   # ROM file path
-        pointersStartAddress = int(sys.argv[3], 16)             # Start offset of pointer table
-        pointerTableSize = int(sys.argv[4], 16)                 # Pointer table size
-        headerSize = int(sys.argv[5], 16)                       # Header size
-        lineBreaker = de.parseLineBreakers(sys.argv[6])         # Parse the line breaker input (comma-separated)
-        outFile = sys.argv[7]                                   # Output file for the extracted text
-        tblFile = sys.argv[8] if len(sys.argv) == 9 else None   # Optional .tbl file argument
+        # Pointers Format
+        if sys.argv[2] == '-2b':
+            pointersFormat = de.processPointers2Bytes
+        elif sys.argv[2] == '-2bs':
+            pointersFormat = de.processPointers2BytesSeparated
+        elif sys.argv[2] == '-2bb':
+            pointersFormat = de.processPointers2BytesBigEndian
+        elif sys.argv[2] == '-3b':
+            pointersFormat = de.processPointers3Bytes
+        elif sys.argv[2] == '-4b':
+            pointersFormat = de.processPointers4Bytes
+        else:
+            sys.stdout.write("Error: Pointers format argument not found.")
+            sys.exit(1) 
+
+        # Other Arguments
+        romFile = sys.argv[3]                                       # ROM file path
+        try:
+            pointersStartAddress = int(sys.argv[4], 16)             # Start offset of pointer table
+        except ValueError:
+            print("Error: Incorrect hex value.")
+            sys.exit(1)
+        try:
+            pointerTableSize = int(sys.argv[5], 16)                 # Pointer table size
+        except ValueError:
+            print("Error: Incorrect hex value.")
+            sys.exit(1)
+        try:
+            headerSize = int(sys.argv[6], 16)                       # Header size
+        except ValueError:
+            print("Error: Incorrect hex value.")
+            sys.exit(1)
+        lineBreaker = sys.argv[7]                                   # Line breaker input (comma-separated)
+        outFile = sys.argv[8]                                       # Output file for the extracted text
+        tblFile = sys.argv[9]                                       # Tbl file argument
 
         # Read ROM pointers table
-        tablePointers = de.readRom(romFile, pointersStartAddress, pointerTableSize)
+        try:
+            tablePointers = de.readRom(romFile, pointersStartAddress, pointerTableSize)
+        except FileNotFoundError:
+            print(f"Error: File {romFile} not found in directory.")
+            sys.exit(1)
 
+        # Parse line breakers.
+        try:
+            parseLineBreakers = de.parseLineBreakers(lineBreaker)
+        except ValueError:
+            print("Error: Incorrect hex value.")
+            sys.exit(1)
+        
         # Process read pointers
-        pointersList = de.processPointers(tablePointers, headerSize)
-
+        lineStartAddress = pointersFormat(tablePointers, headerSize)
+        
         # Read the complete ROM data to extract texts
         romData = de.readRom(romFile, 0, os.path.getsize(romFile))
 
-        # Load the character table if provided
-        charTable = None
-        if tblFile:
-            print("Decoding with TBL")
+        # Load the character table
+        try:
             charTable = de.readTbl(tblFile)
+        except FileNotFoundError:
+            print(f"Error: File {tblFile} not found in directory.")
+            sys.exit(1)
+        except UnicodeDecodeError:
+            print(f"Error: File {tblFile} is not in UTF-8.")
             
         # Extract the texts
-        texts, totalBytesRead = de.extractTexts(romData, pointersList, lineBreaker, charTable)
-
-         # Writing the text to a file
-        de.writeOutFile(outFile, texts)
-        print(f"TEXT BLOCK SIZE: {totalBytesRead} bytes.")
+        try:
+            texts, totalBytesRead, linesLenght = de.extractTexts(romData, lineStartAddress, parseLineBreakers, charTable)
+        except IndexError:
+            print(f"Error: Start address is bigger than the ROM size.")
+            sys.exit(1)
+            
+        # Writing the text to a file
+        de.writeOutFile(outFile, texts, pointersStartAddress, pointerTableSize, lineStartAddress, linesLenght, lineBreaker)
+        print(f"TEXT BLOCK SIZE: {totalBytesRead} / {hex(totalBytesRead)} bytes.")
         print(f"Text extracted to {outFile}")
         print("Decoding complete.\n")
         sys.exit(1)
     
     elif sys.argv[1] == '-e':
-        
-        # Encoding arg.
-        scriptFile = sys.argv[2]                                # Input file with Script
-        textStartAddress = int(sys.argv[3], 16)                 # Start offset of text script
-        textSize = int(sys.argv[4], 16)                         # Text length size
-        pointersStartAddress = int(sys.argv[5], 16)             # Start offset of pointer table
-        headerSize = int(sys.argv[6], 16)                       # Header size
-        romFile = sys.argv[7]                                   # ROM file path
-        tblFile = sys.argv[8] if len(sys.argv) == 9 else None   # Optional .tbl file argument
-
+        # Pointers Format
+        if sys.argv[2] == '-2b':
+            pointersFormat = en.calculatePointer2Bytes
+        elif sys.argv[2] == '-2bs':
+            pointersFormat = en.calculatePointer2BytesSeparated
+        elif sys.argv[2] == '-2bb':
+            pointersFormat = en.calculatePointer2BytesBigEndian
+        elif sys.argv[2] == '-3b':
+            pointersFormat = en.calculatePointer3Bytes
+        elif sys.argv[2] == '-4b':
+            pointersFormat = en.calculatePointer4Bytes
+        else:
+            sys.stdout.write("Error: Pointers format argument not found.")
+            sys.exit(1)
+            
+        # Encoding arguments
+        scriptFile = sys.argv[3]                                    # Input file with Script
+        try:
+            textStartAddress = int(sys.argv[4], 16)                 # Start offset of text script
+        except ValueError:
+            print("Error: Incorrect hex value.")
+            sys.exit(1)
+        try:
+            textSize = int(sys.argv[5], 16)                         # Text length size
+        except ValueError:
+            print("Error: Incorrect hex value.")
+            sys.exit(1)
+        try:
+            pointersStartAddress = int(sys.argv[6], 16)             # Start offset of pointer table
+        except ValueError:
+            print("Error: Incorrect hex value.")
+            sys.exit(1)
+        try:
+            headerSize = int(sys.argv[7], 16)                       # Header size
+        except ValueError:
+            print("Error: Incorrect hex value.")
+            sys.exit(1)
+        romFile = sys.argv[8]                                       # ROM file path
+        tblFile = sys.argv[9]                                       # Tbl file argument
+    
         # Read the text file
-        textScript = en.readScriptFile(scriptFile)
-
+        try:
+            textScript, copyPointersStartAddress, pointersEndAddress, pointerTableSize, lineBreaker = en.readScriptFile(scriptFile)
+        except FileNotFoundError:
+            print(f"Error: File {scriptFile} not found in directory.")
+            sys.exit(1)
+        except AttributeError:
+            print(f"Error: First line attributes not found in {scriptFile}.")
+            sys.exit(1)
+            
+        # Parse line breakers.
+        parseLineBreakers = de.parseLineBreakers(lineBreaker)
+        
         # Load the character table if provided
-        charTable = None
-        if tblFile:
-            print("Encoding with TBL")
-            charTable = en.readTblFileInverted(tblFile)
-
+        try:
+            charTable, longestChar = en.readTblFileInverted(tblFile)
+        except FileNotFoundError:
+            print(f"Error: File {tblFile} not found in directory.")
+            sys.exit(1)
+        except UnicodeDecodeError:
+            print(f"Error: File {tblFile} is not in UTF-8.")
+            
         # Encode the text
-        encodedText, pointersList = en.encodeText(textScript, textStartAddress, charTable)
-
+        encodedText, pointersList = en.encodeText(textScript, parseLineBreakers, charTable, longestChar)
+        
         # Format pointers
-        encodedPointers = en.calculatePointers(pointersList, headerSize)
+        encodedPointers = pointersFormat(pointersList, textStartAddress, headerSize)
 
         # Check that the size of the data does not exceed the maximum allowed
         if len(encodedText) > int(textSize):
             excess = len(encodedText) - int(textSize)
-            sys.stdout.write("ERROR: The number of bytes read exceeds the maximum block limit.\n")
+            sys.stdout.write("Error: The number of bytes read exceeds the maximum block limit.\n")
             sys.stdout.write(f"Remove {excess} bytes from {scriptFile} file.\n") 
             sys.exit(1)
 
@@ -89,14 +178,18 @@ def main():
         freeBytes = int(textSize) - len(encodedText)
             
         # Write the text to the ROM
-        en.writeROM(romFile, textStartAddress, encodedText)
-
+        try:
+            en.writeROM(romFile, textStartAddress, encodedText)
+        except FileNotFoundError:
+            print(f"Error: File {romFile} not found in directory.")
+            sys.exit(1)
+            
         # Write the pointers to the ROM
         en.writeROM(romFile, pointersStartAddress, encodedPointers)
 
         print(f"Text written at offset {hex(textStartAddress)}.")
         print(f"Pointers table written at offset {hex(pointersStartAddress)} with {len(pointersList)} pointers.")
-        print(f"FREE SPACE: {freeBytes} bytes.")
+        print(f"Free space: {freeBytes} bytes.")
         print(f"Data written to {romFile}")
         print("Encoding complete.\n")
         sys.exit(1)
@@ -106,14 +199,22 @@ def main():
         print(" -d  --decode   decode from ROM")
         print(" -e  --encode   encode from raw binary text")
         print(" -h  --help     show help")
-        print(" -v  --version  show version number")
-
+        print(" -v  --version  show version number\n")
+        print(" ****** Pointers Format ****** \n")
+        print(" -2b   --2bytes little endian")
+        print(" -2bb  --2bytes big endian")
+        print(" -2bs  --2bytes splitted lsb-msb")
+        print(" -3b   --3bytes (bank/2bytespointer)")
+        print(" -4b   --4bytes")
+        sys.exit(1)
+        
     elif sys.argv[1] == '-v':
-        print("\nHexSring created by koda, version 1.0")
+        sys.stdout.write("\nHexSring created by koda, version 1.1.0")
+        sys.exit(1)
 
     else:
-        sys.stdout.write("Usage: -d <romFile> <PointersStartAddress> <PointerTableSize> <HeaderSize> <LineBreaker> <outFile> [tblFile]\n")
-        sys.stdout.write("       -e <TextFile> <TextStartAddress> <TextSize> <PointersStartAddress> <HeaderSize> <romFile> [tblFile]\n")
+        sys.stdout.write("Usage: -d <pointersFormat> <romFile> <PointersStartAddress> <PointerTableSize> <HeaderSize> <LineBreaker> <outFile> <tblFile>\n")
+        sys.stdout.write("       -e <pointersFormat> <TextFile> <TextStartAddress> <TextSize> <PointersStartAddress> <HeaderSize> <romFile> <tblFile>\n")
         sys.stdout.write("       -h show help.\n")
         sys.stdout.write("       -v show version.\n")
         sys.exit(1)
